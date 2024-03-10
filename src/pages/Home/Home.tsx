c2b98561f008afef1c841ab6e22235795c4c1a79
draquio <draquio@gmail.com>
2024-03-10 15:36:04 -0400

    agregando analiticas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { Menu } from "../../components/Menu/Menu";
 import { Info } from "../../components/Info/Info";
+import { Analytics } from "@vercel/analytics/react"
 export const Home = () => {
   return (
     <>
     <Menu />
     <Info />
+    <Analytics />
     </>
   );
 };
