ffa62738645bf4b60b21334254548078ff6e33ca
draquio <draquio@gmail.com>
2024-08-08 18:44:33 -0400

    update: removing import of react on Facebook component

--- src/components/Facebook/Facebook.tsx
+++ src/components/Facebook/Facebook.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FacebookI } from "../../interfaces/Interfaces";
 import { VerifyLinkFacebook } from "../../utils/Functions";
 import { Facebook as FacebookClass } from "../../services/facebook";
